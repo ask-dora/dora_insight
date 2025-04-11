4ae1c27dcbee6779110a0de7bd333aba55fbfb0f
Jonathan Rogers <jrogers239@gmail.com>
2025-04-10 22:58:14 -0400

    Firebase first commit

---
+++ frontend/src/firebaseConfig.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIza...",
+    authDomain: "your-project-id.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-project-id.appspot.com",
+    messagingSenderId: "...",
+    appId: "1:...",
+    measurementId: "G-..." // Optional, only if Analytics is enabled
+  };
